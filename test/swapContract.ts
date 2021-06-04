bdc00cb8d3bca6d4207d7c26205f131c6703669a
tcar <tcar121293@gmail.com>
2021-06-04 15:36:52 +0200

    update tests;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- test/swapContract.ts
+++ test/swapContract.ts
@@ -12,6 +12,14 @@ describe("SwapContract", function () {
   let POT: Deployment;
   let Swap: ContractFactory;
 
+  async function deploySwapContract(start: number, end: number, totalDeposit: BigNumber, whitelist: boolean, totalDepositPerUser: BigNumber) {
+    const date = new Date();
+    let startDate = Math.round((date.setDate((date.getDate() + start)) /1000));
+    const endDate = Math.round((date.setDate((date.getDate() + end)) /1000));
+    return await Swap.deploy(startDate, endDate, ethers.utils.parseEther("2"), 
+    ethers.utils.parseEther("5"), totalDeposit, 100, POT.address, whitelist, totalDepositPerUser, POT.address);
+  }
+
   before(async () => {
     signers = await ethers.getSigners();
     Swap = await ethers.getContractFactory("SwapContract");
@@ -20,37 +28,29 @@ describe("SwapContract", function () {
   
   // NEGATIVE TESTS
   it("Should fail if not enough ether provided", async function() {
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() +10)) /1000));
-    const swap = await Swap.deploy(startDate, endDate, 2, 10, 10000, 100, POT.address, false, 1000, POT.address);
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10000"), false, ethers.utils.parseEther("1000"));
 
     await expect(
       signers[0].sendTransaction({
       to: swap.address,
       value: ethers.utils.parseEther("1"),
     })
-    ).to.be.rejectedWith("VM Exception while processing transaction: revert Not enough ether sent");  
+    ).to.be.rejectedWith("VM Exception while processing transaction: revert Invalid deposit amount");  
   });
 
   it("Should fail if too much ether provided", async function() {
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() +10)) /1000));
-    const swap = await Swap.deploy(startDate, endDate, 2, 10, 10000, 100, POT.address, false, 1000, POT.address);
+
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10000"), false, ethers.utils.parseEther("1000"));
     await expect(
       signers[0].sendTransaction({
       to: swap.address,
       value: ethers.utils.parseEther("11"),
     })
-    ).to.be.rejectedWith("VM Exception while processing transaction: revert Not enough ether sent");  
+    ).to.be.rejectedWith("VM Exception while processing transaction: revert Invalid deposit amount");  
   });
 
   it("Should fail if the token sale ended", async function() {
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() - 5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() + 4)) /1000));
-    const swap = await Swap.deploy(startDate, endDate, 2, 10, 10000, 100, POT.address, false, 1000, POT.address);
+    const swap = await deploySwapContract(-5, 4, ethers.utils.parseEther("10000"), false, ethers.utils.parseEther("1000"));
     await expect(
       signers[0].sendTransaction({
       to: swap.address,
@@ -60,10 +60,8 @@ describe("SwapContract", function () {
   });
 
   it("Should fail if the pool token sale did not started yet", async function() {
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() +1)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() +4)) /1000));
-    const swap = await Swap.deploy(startDate, endDate, 2, 10, 10000, 100, POT.address, false, 1000, POT.address);
+    const swap = await deploySwapContract(1, 4, ethers.utils.parseEther("10000"), false, ethers.utils.parseEther("1000"));
+
     await expect(
       signers[0].sendTransaction({
       to: swap.address,
@@ -74,10 +72,7 @@ describe("SwapContract", function () {
 
 
   it("Should fail if all tokens are sold", async function() {
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() +10)) /1000));
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("1000"));
 
     // purchase 500 tokens
     await signers[0].sendTransaction({
@@ -101,11 +96,7 @@ describe("SwapContract", function () {
 
   it("Should fail if you reached the token limit", async function() {
 
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() - 5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() + 10)) /1000));
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 5, POT.address);
-
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("5"));
     await signers[0].sendTransaction({
       to: swap.address,
       value: ethers.utils.parseEther("5"),
@@ -120,12 +111,7 @@ describe("SwapContract", function () {
   });
 
   it("Should fail if user address is not whitelisted", async function() {
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() +10)) /1000));
-
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, true, 1000, POT.address);
-    
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10"), true, ethers.utils.parseEther("1000"));
     await expect(
       signers[0].sendTransaction({
       to: swap.address,
@@ -134,13 +120,8 @@ describe("SwapContract", function () {
     ).to.be.rejectedWith("VM Exception while processing transaction: revert Your address is not whitelisted");
   });
 
-  it("Should successfully buy tokens", async function() {
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() +10)) /1000));
-
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
-    
+  it("Should successfully buy tokens", async function() {    
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("1000"));
     await signers[0].sendTransaction({
     to: swap.address,
     value: ethers.utils.parseEther("3")});
@@ -153,13 +134,7 @@ describe("SwapContract", function () {
 
   it("Should successfully buy tokens when user whitelisted", async function() {
 
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() +10)) /1000));
-
-    // create whitelistable pool
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, true, 1000, POT.address);
-    
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10"), true, ethers.utils.parseEther("1000"));
     // add to the whitelist a user5
     await swap.addToWhitelist([signers[5].getAddress()]);
     // purchase successfuly tokens
@@ -205,8 +180,7 @@ describe("SwapContract", function () {
     const startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
     const endDate = Math.round((date.setDate((date.getDate() +10)) /1000));
 
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
-    
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("1000"));
     await expect(swap.setTimeDates(startDate, endDate))
     .to.be.rejectedWith("VM Exception while processing transaction: revert The pool is already active");
   });
@@ -216,9 +190,13 @@ describe("SwapContract", function () {
     let startDate = Math.round((date.setDate((date.getDate() +1)) /1000));
     const endDate = Math.round((date.setDate((date.getDate() +2)) /1000));
 
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
-    let startTimeValue = BigNumber.from(await swap.provider.getStorageAt(swap.address, 1)).toNumber();
-    let endTimeValue = BigNumber.from(await swap.provider.getStorageAt(swap.address, 2)).toNumber();
+    const swap = await Swap.deploy(startDate, endDate, 2, 5, ethers.utils.parseEther("10"), 
+    100, POT.address, false, ethers.utils.parseEther("1000"), POT.address);
+
+    let startTimeValue = await swap.startTime()
+    startTimeValue = startTimeValue.toNumber()
+    let endTimeValue = await swap.endTime()
+    endTimeValue = endTimeValue.toNumber()
 
     expect(startTimeValue).to.equal(startDate);
     expect(endTimeValue).to.equal(endDate);
@@ -226,55 +204,42 @@ describe("SwapContract", function () {
     const updatedStartDate = Math.round(new Date().getTime() / 1000);
     const updatedEndDate = Math.round(new Date().getTime() / 1000);
     await swap.setTimeDates(updatedStartDate, updatedEndDate);
-    startTimeValue = BigNumber.from(await swap.provider.getStorageAt(swap.address, 1)).toNumber();
-    endTimeValue = BigNumber.from(await swap.provider.getStorageAt(swap.address, 2)).toNumber();
+    startTimeValue = await swap.startTime()
+    startTimeValue = startTimeValue.toNumber()
+    endTimeValue = await swap.endTime()
+    endTimeValue = endTimeValue.toNumber()
     expect(startTimeValue).to.equal(updatedStartDate);
     expect(endTimeValue).to.equal(updatedEndDate);
   });
 
   it("Should successfully change min and max swap amount", async function() {
-    const date = new Date();
-    let startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() + 10)) /1000));
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("1000"));
+    let minSwap = await swap.minSwapAmount();
+    let maxSwap = await swap.maxSwapAmount();
+    expect(minSwap).to.deep.equal(ethers.utils.parseEther("2"));
+    expect(maxSwap).to.deep.equal(ethers.utils.parseEther("5"));
 
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
-    let minSwap = BigNumber.from(await swap.provider.getStorageAt(swap.address, 3)).toString();
-    let maxSwap = BigNumber.from(await swap.provider.getStorageAt(swap.address, 4)).toString();
-
-    expect(minSwap).to.equal((2 * Math.pow(10,18)).toString());
-    expect(maxSwap).to.equal((5 * Math.pow(10,18)).toString());
-
-
-    await swap.setLimits(1, 10);
-    minSwap = BigNumber.from(await swap.provider.getStorageAt(swap.address, 3)).toString();
-    maxSwap = BigNumber.from(await swap.provider.getStorageAt(swap.address, 4)).toString();
-    expect(minSwap).to.equal((1 * Math.pow(10,18)).toString());
-    expect(maxSwap).to.equal((10 * Math.pow(10,18)).toString());
+    await swap.setLimits(ethers.utils.parseEther("1"), ethers.utils.parseEther("10"));
+    minSwap = await swap.minSwapAmount();
+    maxSwap = await swap.maxSwapAmount();
+    expect(minSwap).to.deep.equal(ethers.utils.parseEther("1"));
+    expect(maxSwap).to.deep.equal(ethers.utils.parseEther("10"));
   });
 
   it("Should successfully change token address", async function() {
-    const date = new Date();
-    let startDate = Math.round((date.setDate((date.getDate() + 5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() + 10)) /1000));
+    const swap = await deploySwapContract(5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("1000"));
 
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
-
-    let tokenAdd = await ethers.utils.hexStripZeros(await swap.provider.getStorageAt(swap.address, 6));
-
-    expect(tokenAdd).to.equal(POT.address.toLowerCase());
+    let tokenAdd = await swap.token();
+    expect(tokenAdd).to.equal(POT.address);
     const newAdd = await signers[3].getAddress();
 
     await swap.setTokenAddress(newAdd);
-    tokenAdd = await ethers.utils.hexStripZeros(await swap.provider.getStorageAt(swap.address, 6));
-    expect(tokenAdd).to.equal(newAdd.toLowerCase());
+    tokenAdd = await swap.token();
+    expect(tokenAdd).to.equal(newAdd);
   });
 
   it("Should revert when trying to update tokenAddress if pool already active", async function() {
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() +10)) /1000));
-
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("1000"));
     const newAdd = await signers[3].getAddress();
 
     await expect(
@@ -284,45 +249,37 @@ describe("SwapContract", function () {
   });
 
   it("Should successfully change swap price", async function() {
-    const date = new Date();
-    let startDate = Math.round((date.setDate((date.getDate() +5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() + 10)) /1000));
-
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
-    let swapPrice = BigNumber.from(await swap.provider.getStorageAt(swap.address, 5)).toNumber();
+    const swap = await deploySwapContract(5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("1000"));
+    let swapPrice = await swap.swapPrice();
+    swapPrice = swapPrice.toNumber();
 
     expect(swapPrice).to.equal(100);
 
-
     await swap.setSwapPrice(500);
-    swapPrice = BigNumber.from(await swap.provider.getStorageAt(swap.address, 5)).toNumber();
+    swapPrice = await swap.swapPrice();
+    swapPrice = swapPrice.toNumber();
+
     expect(swapPrice).to.equal(500);
   });
 
   it("Should revert when trying to update swap price if pool already active", async function() {
-    const date = new Date();
-    const startDate = Math.round((date.setDate((date.getDate() -5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() +10)) /1000));
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
+    const swap = await deploySwapContract(-5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("1000"));
 
     await expect(swap.setSwapPrice(500))
     .to.be.rejectedWith("VM Exception while processing transaction: revert The pool is already active");
   });
 
   it("Should successfully change vesting contract address", async function() {
-    const date = new Date();
-    let startDate = Math.round((date.setDate((date.getDate() + 5)) /1000));
-    const endDate = Math.round((date.setDate((date.getDate() + 10)) /1000));
-    const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
+    const swap = await deploySwapContract(5, 10, ethers.utils.parseEther("10"), false, ethers.utils.parseEther("1000"));
 
-    let vestingAdd = await ethers.utils.hexStripZeros(await swap.provider.getStorageAt(swap.address, 9));
+    let vestingAdd = await swap.vestingContract();
 
-    expect(vestingAdd).to.equal(POT.address.toLowerCase());
+    expect(vestingAdd).to.equal(POT.address);
     const newAdd = await signers[3].getAddress();
 
     await swap.setVestingContract(newAdd);
-    vestingAdd = await ethers.utils.hexStripZeros(await swap.provider.getStorageAt(swap.address, 9));
-    expect(vestingAdd).to.equal(newAdd.toLowerCase());
+    vestingAdd = await swap.vestingContract();
+    expect(vestingAdd).to.equal(newAdd);
   });
 
   it("Should revert when trying to update tokenAddress if pool already active", async function() {
@@ -331,6 +288,7 @@ describe("SwapContract", function () {
     const endDate = Math.round((date.setDate((date.getDate() + 10)) /1000));
 
     const swap = await Swap.deploy(startDate, endDate, 2, 5, 10, 100, POT.address, false, 1000, POT.address);
+
     const swapContract = (await ethers.getContractAt(
       "SwapContract",
       swap.address,
@@ -385,7 +343,6 @@ describe("SwapContract", function () {
     );
   });
 });
-
 
 
 
