74e515d6fbf099a8896af418a2bf101480a423c3
tcar <tcar121293@gmail.com>
2021-07-09 09:32:13 +0200

    optimize code;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- contracts/Whitelisted.sol
+++ contracts/Whitelisted.sol
@@ -3,24 +3,21 @@ pragma solidity ^0.8.1;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Whitelisted is Ownable{
-    mapping (address => mapping (string => bool)) internal whitelisted;
+    mapping (address => bool) internal whitelisted;
 
     /// @param ethAddress users ethereum account address
-    /// @param substrateAddress users statemint account address
-    function addToWhitelist(address ethAddress, string memory substrateAddress) external onlyOwner{
-        whitelisted[ethAddress][substrateAddress] = true;
+    function addToWhitelist(address ethAddress) external onlyOwner{
+        whitelisted[ethAddress] = true;
     }
 
     /// @param ethAddress users ethereum account address
-    /// @param substrateAddress users statemint account address
-    function removeFromWhitelist(address ethAddress, string memory substrateAddress) external onlyOwner{
-        delete whitelisted[ethAddress][substrateAddress];
+    function removeFromWhitelist(address ethAddress) external onlyOwner{
+        delete whitelisted[ethAddress];
     }
 
     /// @param ethAddress users ethereum account address
-    /// @param substrateAddress users statemint account address
     /// @return true if the user is whitelisted, otherwise false
-    function isWhitelisted(address ethAddress, string memory substrateAddress) view external returns(bool){
-        return whitelisted[ethAddress][substrateAddress];
+    function isWhitelisted(address ethAddress) view external returns(bool){
+        return whitelisted[ethAddress];
     }
 }
